d039d254d14d19369508c0e704e4e5629121e106
Ibrahim Ahmed <abeahmed2@gmail.com>
2023-06-12 18:21:18 -0700

    Upgrade SDK

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,26 +1,27 @@
-import { Leap } from "@leap-ai/sdk";
+import { GenerateStateEnum, GenerateUpscaleEnum, Leap } from "@leap-ai/sdk";
 import { NextApiRequest, NextApiResponse } from "next";
 import { nsfwCheck } from "../../helpers/nsfw";
+
+// check for api key
+const api_key = process.env.API_KEY as string;
+
+if (!api_key) {
+  throw new Error("API_KEY environment variable not set.");
+}
+
+// instantiate sdk
+const leap = new Leap({
+  apiKey: api_key,
+});
+const generatorService = leap.images.generate;
 
 const generate = async (req: NextApiRequest, res: NextApiResponse) => {
   // parse the `body` parameter for apiKey and prompt
   const { prompt } = req.body;
 
-  // check for api key
-  const api_key = process.env.API_KEY as string;
-
-  if (!api_key) {
-    res.status(400).json({ error: "Invalid request. Check API Key" });
-    return;
-  }
-
-  // instantiate sdk
-  const leap = new Leap(api_key);
-
   let images = <string[]>[];
 
   const { isNsfw } = nsfwCheck(prompt);
-
   if (isNsfw) {
     res.status(400).json({
       error: "NSFW prompt detected. Please try again with a different prompt.",
@@ -28,30 +29,52 @@ const generate = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const random = Math.floor(Math.random() * 100);
-  const { data: image, error: imageError } = await leap.generate.generateImage({
-    prompt: prompt,
-    numberOfImages: 1,
-    steps: 30,
-    upscaleBy: "x1",
-    height: 384,
-    width: 832,
-    modelId:
-      random > 0 && random <= 30
-        ? "8b1b897c-d66d-45a6-b8d7-8e32421d02cf" // SD 1.5
-        : random > 30 && random < 65
-        ? "eab32df0-de26-4b83-a908-a83f3015e971	" // realistic vision
-        : "ee88d150-4259-4b77-9d0f-090abe29f650	", // SD 2.1
+  const modelId =
+    random > 0 && random <= 30
+      ? "8b1b897c-d66d-45a6-b8d7-8e32421d02cf" // SD 1.5
+      : random > 30 && random < 65
+      ? "eab32df0-de26-4b83-a908-a83f3015e971	" // realistic vision
+      : "ee88d150-4259-4b77-9d0f-090abe29f650	"; // SD 2.1
+  const result = await generatorService.createImages({
+    parameters: {
+      prompt: prompt,
+      numberOfImages: 1,
+      steps: 30,
+      upscaleBy: GenerateUpscaleEnum.X1,
+      height: 384,
+      width: 832,
+    },
+    modelId,
   });
 
-  if (imageError) {
-    res.status(500).json(imageError);
-    return;
-  }
+  let attempts = 0;
+  while (true) {
+    if (attempts > 10) {
+      return res.status(500).json({
+        error: "Failed to generate images, please try again.",
+      });
+    }
 
-  if (image) {
-    image.images.forEach((image) => {
-      images.push(image.uri);
+    const latestResult = await generatorService.findById({
+      jobId: result.id,
+      modelId,
     });
+
+    if (latestResult.state === GenerateStateEnum.FINISHED) {
+      latestResult.images.forEach((image) => {
+        images.push(image.uri);
+      });
+      break;
+    }
+
+    if (latestResult.state === GenerateStateEnum.FAILED) {
+      return res.status(500).json({
+        error: "Failed to generate images, please try again.",
+      });
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    attempts++;
   }
 
   res.status(200).json({ images: images });
